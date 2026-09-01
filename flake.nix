d36754d23b9671bbff274693ba0d7ac868d30c0d
Surya Vamsi <d.suryavamsi@gmail.com>
2026-09-01 22:44:46 +0530

    Expose a NixOS module

--- flake.nix
+++ flake.nix
@@ -8,6 +8,27 @@
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
     in {
+      nixosModules.default = { config, lib, pkgs, ... }:
+        let
+          cfg = config.programs.pi-stats-dashboard;
+          piPackage = pkgs.runCommand "pi-stats-dashboard-package" {} ''
+            mkdir -p $out/share/pi-stats-dashboard
+            cp -r ${cfg.package}/. $out/share/pi-stats-dashboard/
+          '';
+        in {
+          options.programs.pi-stats-dashboard = {
+            enable = lib.mkEnableOption "the Pi stats dashboard package";
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = self.packages.${pkgs.system}.default;
+              description = "Pi stats dashboard package to expose to Pi.";
+            };
+          };
+          config = lib.mkIf cfg.enable {
+            environment.systemPackages = [ piPackage ];
+          };
+        };
+
       packages = forAllSystems (pkgs: {
         default = pkgs.stdenvNoCC.mkDerivation {
           pname = "pi-stats-dashboard";
@@ -20,7 +41,6 @@
           '';
         };
       });
-
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
           packages = [ pkgs.nodejs_22 ];
